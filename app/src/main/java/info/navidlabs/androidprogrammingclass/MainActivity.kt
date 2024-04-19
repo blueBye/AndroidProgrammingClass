5b50324e5a678dfd8dd1014212a86e58e7f176c8
blueBye <navidshirmohammadi81@gmail.com>
2024-04-19 12:39:42 +0330

    Card styling

--- app/src/main/java/info/navidlabs/androidprogrammingclass/MainActivity.kt
+++ app/src/main/java/info/navidlabs/androidprogrammingclass/MainActivity.kt
@@ -3,31 +3,31 @@ package info.navidlabs.androidprogrammingclass
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.paddingFromBaseline
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Home
-import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material.icons.filled.Face
+import androidx.compose.material3.Card
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Brush
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -38,8 +38,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             AndroidProgrammingClassTheme {
-                BoxFunction()
-                ButtonFunction()
+                CardImageFunction()
             }
         }
     }
@@ -52,85 +51,77 @@ class MainActivity : ComponentActivity() {
     showSystemUi = true,
 )
 @Composable
-private fun BoxFunction() {
-    Box(modifier= Modifier
-        .fillMaxSize()
-        .background(Color.LightGray)
-    ) {
-        Box(modifier= Modifier
-            .size(100.dp, 100.dp)
-            .background(Color.Red)
-        )
-
-        Box(modifier= Modifier
-            .size(200.dp, 200.dp)
-            .background(Color.Cyan)
-            .align(Alignment.Center)
-        )
-
-        Box(modifier= Modifier
-            .size(300.dp, 300.dp)
-            .background(brush= Brush.verticalGradient(
-                colors = listOf(
-                    MaterialTheme.colorScheme.primary,
-                    Color.Red,
-                )
-            ))
-            .align(Alignment.BottomEnd)
-        )
-    }
-}
-
-@Preview(
-    showBackground = true,
-    device = "id:pixel_7_pro",
-    showSystemUi = true,
-    name = "Second View",
-)
-@Composable
-private fun ButtonFunction() {
+private fun CardImageFunction() {
     Column(
-        modifier=Modifier.fillMaxSize(),
-        horizontalAlignment=Alignment.CenterHorizontally,
-        verticalArrangement=Arrangement.Center,
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
     ) {
-        Button(onClick={/*onClick*/}) {
-            Text(text="Click Me 1")
-        }
-
-        Button(
-            onClick={/*onClick*/},
-            colors=ButtonDefaults.buttonColors(containerColor=Color.Blue),
+        // card 1
+        Card(
+            shape=RoundedCornerShape(size=5.dp),
+            modifier=Modifier.padding(8.dp),
         ) {
-            Text(text="Click Me 2")
+            Row(
+                modifier=Modifier.padding(all=12.dp),
+                verticalAlignment=Alignment.CenterVertically,
+                horizontalArrangement=Arrangement.Center,
+            ) {
+                Icon(imageVector = Icons.Default.Face, contentDescription = null)
+
+                Spacer(modifier=Modifier.width(8.dp))
+                Text(
+                    text="alice",
+                    fontSize=16.sp,
+                    fontWeight=FontWeight.Medium,
+                )
+            }
         }
 
-        Button(onClick={/*onClick*/}) {
-            Text(text="Click", color=Color.Yellow)
-            Text(text="Me", color=Color.Red)
-        }
-
-        Button(onClick={/*onClick*/}) {
-            Icon(
-                imageVector = Icons.Default.Home,
-                contentDescription = null,
-            )
-            Text(text="Icon!", color=Color.Red)
-        }
-
-        Button(
-            onClick={/*onClick*/},
-            shape= RoundedCornerShape(12.dp),
+        //card 2
+        Card (
+            shape=RoundedCornerShape(4.dp),
+            modifier=Modifier.padding(8.dp),
         ) {
-            Text(text="Custom Corner", color=Color.Yellow)
+            Row(
+                verticalAlignment=Alignment.CenterVertically,
+                modifier=Modifier.width(200.dp),
+            ) {
+                Icon(imageVector = Icons.Default.Face, contentDescription = null)
+                Text(
+                    text="bob",
+                    style=MaterialTheme.typography.headlineSmall,
+                    modifier=Modifier.padding(horizontal=12.dp),
+                )
+            }
         }
 
-        Button(
-            onClick={/*onClick*/},
-            border= BorderStroke(1.dp, color=Color.Blue),
-            colors= ButtonDefaults.outlinedButtonColors(contentColor=Color.Blue),
+        // card 3
+        Card(
+            shape=RoundedCornerShape(4.dp),
+            modifier=Modifier.padding(8.dp),
         ) {
-            Text(text="Custom Corner")
+            Column(
+                modifier= Modifier
+                    .padding(8.dp)
+                    .width(100.dp),
+                horizontalAlignment=Alignment.CenterHorizontally,
+            ) {
+                Image(
+                    painter=painterResource(id=R.drawable.filotecnia_s_store___society6),
+                    contentDescription=null,
+                    modifier=Modifier.clip(shape=CircleShape).size(60.dp),
+                    contentScale= ContentScale.Crop,
+                )
+                Text(
+                    text="Hello Cards!",
+                    modifier=Modifier
+                        .paddingFromBaseline(
+                            top=40.dp,
+                        ),
+                    fontWeight= FontWeight.Medium,
+                )
+            }
         }
     }
 }
