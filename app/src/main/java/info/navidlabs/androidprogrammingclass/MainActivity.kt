59a45923d6a51a34c8694eee8aa2e48caf09ece4
blueBye <navidshirmohammadi81@gmail.com>
2024-04-22 16:39:42 +0330

    basic encryption

--- app/src/main/java/info/navidlabs/androidprogrammingclass/MainActivity.kt
+++ app/src/main/java/info/navidlabs/androidprogrammingclass/MainActivity.kt
@@ -1,6 +1,8 @@
 package info.navidlabs.androidprogrammingclass
 
 import android.os.Bundle
+import android.util.Base64
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
@@ -11,20 +13,52 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import info.navidlabs.androidprogrammingclass.ui.theme.AndroidProgrammingClassTheme
+import javax.crypto.Cipher
+import javax.crypto.KeyGenerator
+import javax.crypto.SecretKey
+import javax.crypto.spec.IvParameterSpec
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             AndroidProgrammingClassTheme {
-                // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Text(text = "Hello Android")
+                    val k = generateAESKey()
+                    val p = "Hello Android"
+                    val c = aesEncrypt(p.toByteArray(), k)
+                    val d = aesDecrypt(c, k)
+
+                    Log.i("AES", "k: ${Base64.encodeToString(k.encoded, 0)}")
+                    Log.i("AES", "c: ${Base64.encodeToString(c, 0)}")
+                    Log.i("AES", "d: ${Base64.encodeToString(d, 0)}")
+
+                    Text(text = d.toString())
                 }
             }
         }
+    }
+
+    fun generateAESKey(keySize: Int = 256): SecretKey {
+        val keyGenerator = KeyGenerator.getInstance("AES")
+        keyGenerator.init(keySize)
+        return keyGenerator.generateKey()
+    }
+
+    fun aesEncrypt(data: ByteArray, secretKey: SecretKey): ByteArray {
+        val cipher = Cipher.getInstance("AES/CBC/PKCS5Padding")
+        val ivParameterSpec = IvParameterSpec(ByteArray(16))
+        cipher.init(Cipher.ENCRYPT_MODE, secretKey, ivParameterSpec)
+        return cipher.doFinal(data)
+    }
+
+    fun aesDecrypt(encryptedData: ByteArray, secretKey: SecretKey): ByteArray {
+        val cipher = Cipher.getInstance("AES/CBC/PKCS5Padding")
+        val ivParameterSpec = IvParameterSpec(ByteArray(16))
+        cipher.init(Cipher.DECRYPT_MODE, secretKey, ivParameterSpec)
+        return cipher.doFinal(encryptedData)
     }
 }
